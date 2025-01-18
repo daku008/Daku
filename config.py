41b74932e42c6885f249b5a283dea02c074e2978
daku008 <dakub2238@gmail.com>
2025-01-19 02:31:28 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY EVIL4REAL
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8156506636:AAEZcWK3bfXBYdZhpXOcwI893hrburB1D9g")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@DAKUBhaiZz")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1944182800")
+
+
+
+
+
+
+
+
