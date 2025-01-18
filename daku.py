ec036709f370714d8038d00a978eeac99be81707
daku008 <dakub2238@gmail.com>
2025-01-19 02:08:25 +0530

    Update daku.py

--- daku.py
+++ daku.py
@@ -9,10 +9,10 @@ attack_in_progress = False
 current_attack = None  # Store details of the current attack
 attack_history = []  # Store attack logs
 
-TELEGRAM_BOT_TOKEN = '7960283920:AAGtDd0DxtMjTM0Xi8TQgulpFMtCkmhavls'
+TELEGRAM_BOT_TOKEN = '8156506636:AAEZcWK3bfXBYdZhpXOcwI893hrburB1D9g'
 ADMIN_USER_ID = 1944182800
 MONGO_URI = "mongodb+srv://Kamisama:Kamisama@kamisama.m6kon.mongodb.net/"
-DB_NAME = "dmklke"
+DB_NAME = "deepak"
 COLLECTION_NAME = "users"
 attack_in_progress = False
 ATTACK_TIME_LIMIT = 240  # Maximum attack duration in seconds
